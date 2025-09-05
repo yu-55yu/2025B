69d04a291b71fa48909a31bcdf4aa1e0329f2be2
yu-55yu <2761175290@qq.com>
2025-09-05 15:13:22 +0800

    修改了细小参数和注释

--- ADC_FFT_Get_Wave_Mes.m
+++ ADC_FFT_Get_Wave_Mes.m
@@ -3,17 +3,16 @@ function [VPP, Freq] = ADC_FFT_Get_Wave_Mes(Row, Fs, FFT_Output, correctNum, FFT
     DatePower1 = 0;
     DatePower2 = 0;
     
-    % 在峰值周围±correctNum范围内计算能量重心
     for i = -correctNum:correctNum
         idx = Row + i;
-        if idx >= 1 && idx <= length(FFT_Output)  % MATLAB索引从1开始，添加边界检查
+        if idx >= 1 && idx <= length(FFT_Output) 
             power = FFT_Output(idx) * FFT_Output(idx);
-            DatePower1 = DatePower1 + idx * power;  % 用索引加权
+            DatePower1 = DatePower1 + idx * power;
             DatePower2 = DatePower2 + power;
         end
     end
     
-    f = DatePower1 / DatePower2;  % 得到加权平均索引
-    Freq = f * Fs / FFT_LEN;      % 校正后的频率
-    VPP = 2.0 * sqrt(k * DatePower2);  % 幅值
+    f = DatePower1 / DatePower2;
+    Freq = f * Fs / FFT_LEN; 
+    VPP = 2.0 * sqrt(k * DatePower2); 
 end
